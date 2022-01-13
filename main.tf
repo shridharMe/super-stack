58272224474a883e10e9263307f3fa32177d0817
Shridhar <36041359+shridharMe@users.noreply.github.com>
2022-01-13 18:56:32 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "demo-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
@@ -64,8 +64,8 @@ module "cluster" {
   apply_immediately   = true
   monitoring_interval = 10
 
-  db_parameter_group_name         = "default"
-  db_cluster_parameter_group_name = "default"
+  db_parameter_group_name         = aws_db_parameter_group.db_parameter.id
+  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.cluster_parameter_group.id
 
   enabled_cloudwatch_logs_exports = ["postgresql"]
 
@@ -74,8 +74,31 @@ module "cluster" {
     Terraform   = "true"
   }
 }
+
+resource "aws_db_parameter_group" "db_parameter" {
+  name        = "aurora-db-postgres11-parameter-group"
+  family      = "aurora-postgresql11"
+  description = "aurora-db-postgres11-parameter-group"
+  tags        = local.tags
+}
+
+resource "aws_rds_cluster_parameter_group" "cluster_parameter_group" {
+  name        = "aurora-postgres11-cluster-parameter-group"
+  family      = "aurora-postgresql11"
+  description = "aurora-postgres11-cluster-parameter-group"
+  tags        = local.tags
+}
+
 variable "region" {
   description = "aws region"
   type        = string
   default     = "us-east-1"
 }
+
+locals {
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+}
+
